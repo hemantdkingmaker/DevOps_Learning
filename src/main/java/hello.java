17a0389f41a82767f362c161579788a09df413df
hemantdkingmaker <hemantkumar.yaranagula@gmail.com>
2020-05-26 16:00:30 +0530

    adding maven files

---
+++ src/main/java/hello.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import hello.Greeter;
+
+//package hello;
+
+public class hello {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
